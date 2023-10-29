793fd1af265ec23e2d66fc48acc321d5b0233c26
KD-ayoub <ayoubkadi285@gmail.com>
2023-10-29 17:28:10 +0100

    working on light in top right side

--- src/app/(pages)/game/page.tsx
+++ src/app/(pages)/game/page.tsx
@@ -3,10 +3,10 @@ import { Header, SideBar } from "@/app/components";
 export default function Game() {
   return (
     <main className="h-screen bg-[#0B0813] relative w-full max-w-[2880px] flex">
-      <div className="h-[80px] w-[80px] rounded-full fixed top-0 right-0 bg-gradient-to-b from-[#323138] via-[#E95A3A] to-[#60C58D] blur-3xl "></div>
+      <div className="h-[80px] w-[80px] sm:w-28 sm:h-28 md:w-64 md:h-64 lg:w-80 lg:h-80 rounded-full fixed -top-5 sm:-top-10 md:-top-32 lg:-top-40 right-0 opacity-70 sm:opacity-60 md:opacity-30 lg:opacity-25 bg-gradient-to-b from-[#323138] via-[#E95A3A] to-[#60C58D] blur-3xl "></div>
       <Header />
       <SideBar />
-      <div className=" grow overflow-y-auto mt-[41px] sm:mt-11 md:mt-14 flex justify-center items-center">
+      <div className=" grow overflow-y-auto mt-[41px] sm:mt-11 md:mt-14 lg:mt-[72px] //flex justify-center items-center//">
         {/* <div className="h-[200px] w-[70px] bg-red-500"></div> */}
       </div>
     </main>
