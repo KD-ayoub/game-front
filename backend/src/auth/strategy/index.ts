5a663998493d3c89876fd4a88ad81bee3e894ab7
Younes Askour <pausedarkf@gmail.com>
2023-11-11 23:06:46 +0100

    remove credentials from source code

--- backend/src/auth/strategy/index.ts
+++ backend/src/auth/strategy/index.ts
@@ -9,8 +9,8 @@ import { User } from "@prisma/client";
 export class FT_Strategy extends PassportStrategy(Strategy, '42') {
     constructor(private readonly authService: AuthService){
         super({
-            clientID: 'u-s4t2ud-e022f6535cf6a37e0d50bd177dd7e958bedba460d8c8da455b3a6f1e320c57a9', 
-            clientSecret: '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',
+            clientID: process.env.CLIENTID, 
+            clientSecret: process.env.CLIENT_SECRET,
 			callbackURL: '/auth/42/redirect',
         });
     }
