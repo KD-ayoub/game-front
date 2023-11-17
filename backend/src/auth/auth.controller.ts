936f267b1c122eb2ee0c36a1bfd0006df808c243
Younes Askour <pausedarkf@gmail.com>
2023-11-17 21:57:26 +0100

    add logout route

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { AuthenticatedGuard, FT_GUARD, first_timeGuard } from './guards';
 import { AuthService } from './auth.service';
 import { intra_api_info, signup, user_request } from 'src/utils/types';
+import { resolve } from 'path';
 
 @Controller('auth')
 export class AuthController {
@@ -15,15 +16,14 @@ export class AuthController {
 	  return;
 	}
 
-
 	// 42 api redirect to this page and this page send a cookie
 	@Get('/redirect')
 	@UseGuards(FT_GUARD)
-	redirect() {
+	redirect() 
+	{
 		// return here if first time
 		return 1;
 	}
-
 
 	// this route get user info for the first time	
 	@Post('/signup')
@@ -32,17 +32,24 @@ export class AuthController {
 	  return this.auth.signup(req.user,body);
 	}
 	
-	
 	// guards after checking 42 login and then check if first time and then check for 2fa
 	@Get('/status')
 	@UseGuards(first_timeGuard)
-	status(@Req() req: Request) {
+	status(@Req() req: Request) 
+	{
 	  return req.user;
 	}
 	
 	@Get('/logout')
 	@UseGuards(first_timeGuard)
 	logout(@Req() req: Request) {
+		req.session.destroy((err : any) => {
+			if (err)
+			{
+				throw err;
+			}
+			resolve();
+		});
 	  return req.user;
 	}
 
@@ -51,5 +58,12 @@ export class AuthController {
 	test()
 	{
 		return "test";
+	}
+
+
+	@Post('2fa')
+	_2fa(@Req() req: Request,@Body() body: signup)
+	{
+		return req.user;
 	}
 }
