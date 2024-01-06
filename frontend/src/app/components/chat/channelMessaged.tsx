9a396ac648e0cc87a118afb37559a4d0200ac235
Aissa Kouame <kouamissa@outlook.com>
2024-01-06 18:56:41 +0100

    UP

--- frontend/src/app/components/chat/channelMessaged.tsx
+++ frontend/src/app/components/chat/channelMessaged.tsx
@@ -157,13 +157,14 @@ export default function ChannelMessaged({
                         <Modal.Header />
                         <Modal.Body>
                           <div className="text-center">
-                            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                            {/* <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" /> */}
                             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
                               Are you sure you want to join this channel?
                             </h3>
                             <div className="flex justify-center gap-4">
                               <Button
-                                color="failure"
+                                // color="failure"
+                                className="bg-[#E95A3A]"
                                 onClick={() => {
                                   joinPublicChannel(channelSelected.id);
                                   setOpenModal(false)
